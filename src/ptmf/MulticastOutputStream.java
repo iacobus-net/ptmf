fffae4dbd0f6f4cd9dd1b644d5e202ac9fbeccdb
Alejandro <alejandro@iacobus.net>
2015-04-29 19:54:46 +0200

    Update licence to Apache 2.0

--- src/ptmf/MulticastOutputStream.java
+++ src/ptmf/MulticastOutputStream.java
@@ -1,6 +1,6 @@
 //============================================================================
 //
-//	Copyright (c) 1999 . All Rights Reserved.
+//	Copyright (c) 1999-2015 . All Rights Reserved.
 //
 //----------------------------------------------------------------------------
 //
@@ -9,24 +9,25 @@
 //	Description: Clase MulticastOutputStream.
 //
 // 	Authors: 
-//		 Alejandro Garcia Dominguez (alejandro.garcia.dominguez@gmail.com)
+//		 Alejandro García-Domínguez (alejandro.garcia.dominguez@gmail.com)
 //		 Antonio Berrocal Piris (antonioberrocalpiris@gmail.com)
 //
+//  Historial: 
+//  07.04.2015 Changed licence to Apache 2.0     
 //
-//      This file is part of PTMF 
+//  This file is part of PTMF 
 //
-//      PTMF is free software: you can redistribute it and/or modify
-//      it under the terms of the Lesser GNU General Public License as published by
-//      the Free Software Foundation, either version 3 of the License, or
-//      (at your option) any later version.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//      PTMF is distributed in the hope that it will be useful,
-//      but WITHOUT ANY WARRANTY; without even the implied warranty of
-//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//      Lesser GNU General Public License for more details.
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
-//      You should have received a copy of the Lesser GNU General Public License
-//      along with PTMF.  If not, see <http://www.gnu.org/licenses/>.
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //----------------------------------------------------------------------------
 
 
